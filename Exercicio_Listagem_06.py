1d00b8ff8766c0d14ca617a274cc210cfdfc6ba1
Bellafarias <pereira.isabellafarias@gmail.com>
2023-04-13 10:41:38 -0300

    realizada a conclusão do exercicio 6, demontrando o número de alunos aprovados a petir das notas

--- Exercicio_Listagem_06.py
+++ Exercicio_Listagem_06.py
@@ -1,6 +1,10 @@
 Notinhas = [['Leticia', [10, 9, 8, 9]] , ['Maykon', [7, 9, 5, 1]] , ['Mayara', [6, 6, 8, 10]] , ['Gabriela', [7, 8, 7, 4]] , ['Laura', [7, 6, 3, 2]] , ['Natália', [2, 7, 9, 9]] , ['Cintia', [3, 8, 9, 10]] , ['Lorena', [3, 9, 6, 2]] , ['Eduardo', [2, 1, 5, 3]] , ['Henrique', [1, 5, 9, 4]]]
-
+NotasAzul = []
 for estudante in Notinhas:
     notas = estudante[1]
     media = sum(notas) / len(notas)
     print(f"A média das notas de {estudante[0]} é {media:.2f}")
+    if media >= 7:
+        NotasAzul.append(media)     
+
+print ("Aprovados:", len(NotasAzul))
