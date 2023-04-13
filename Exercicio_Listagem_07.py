dae610c00f9f76b997e137d1d697bb09c1afc086
Bellafarias <pereira.isabellafarias@gmail.com>
2023-04-13 10:55:21 -0300

    Foi realizado o exercicio 7, com as funções já utilizadas anteriormnete para multiplicação e soma de números inteiros

---
+++ Exercicio_Listagem_07.py
@@ -0,0 +1,11 @@
+CoolNumber = [ 6, 7, 8, 9, 10]
+soma = 0 
+multiplicação = 1
+for valores in CoolNumber:
+    soma = soma + valores
+    multiplicação = multiplicação * valores
+    
+print ("Números selecionados:", CoolNumber)
+print("Soma dos Valores:", soma)
+print("multiplicação dos Valores:", multiplicação)
+
